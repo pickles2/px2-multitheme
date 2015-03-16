e8c6f820faed19049b2cdf961460b9109cb5328d
Tomoya Koyanagi <tomk79@gmail.com>
2015-03-16 16:55:46 +0900

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+px2-multitheme
+==============
+
+*px2-multitheme* は、[Pickles2](http://pickles2.pxt.jp/) に、複数のテーマを同時に管理、スイッチする機能を追加します。
+
+
+## ライセンス - License
+
+MIT License
+
+
+## 作者 - Author
+
+- (C)Tomoya Koyanagi <tomk79@gmail.com>
+- website: <http://www.pxt.jp/>
+- Twitter: @tomk79 <http://twitter.com/tomk79/>
+
+
+## 開発者向け情報 - for Developer
+
+### テスト - Test
+
+```
+$ ./vendor/phpunit/phpunit/phpunit px-files/tests/mainTest.php main
+```
---
+++ px-files/tests/mainTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * test
+ */
+
+class mainTest extends PHPUnit_Framework_TestCase{
+
+	/**
+	 * ファイルシステムユーティリティ
+	 */
+	private $fs;
+
+	/**
+	 * setup
+	 */
+	public function setup(){
+		$this->fs = new \tomk79\filesystem();
+	}
+
+	/**
+	 * Px2を実行してみる
+	 */
+	public function testMain(){
+
+		// トップページを実行
+		$output = $this->passthru( [
+			'php',
+			__DIR__.'/../../.px_execute.php' ,
+			'/' ,
+		] );
+
+		// var_dump($output);
+		$this->assertTrue( gettype($output) == gettype('') );
+
+	}//testPx2MultiTheme()
+
+
+
+
+	/**
+	 * コマンドを実行し、標準出力値を返す
+	 * @param array $ary_command コマンドのパラメータを要素として持つ配列
+	 * @return string コマンドの標準出力値
+	 */
+	private function passthru( $ary_command ){
+		$cmd = array();
+		foreach( $ary_command as $row ){
+			$param = '"'.addslashes($row).'"';
+			array_push( $cmd, $param );
+		}
+		$cmd = implode( ' ', $cmd );
+		ob_start();
+		passthru( $cmd );
+		$bin = ob_get_clean();
+		return $bin;
+	}// passthru()
+
+}
