8caa9c687f02719a8c5298fb0f6f2709d768cd51
Tomoya Koyanagi <tomk79@gmail.com>
2025-03-20 13:51:28 +0900

    開発環境整備

--- tests/testdata/standard/px-files/config.php
+++ tests/testdata/standard/px-files/config.php
@@ -33,24 +33,37 @@ return call_user_func( function(){
 	$conf->session_name = 'PXSID';
 	$conf->session_expire = 1800;
 	$conf->allow_pxcommands = 1; // PX Commands のウェブインターフェイスからの実行を許可
+	$conf->default_timezone = 'Asia/Tokyo';
+
+	$conf->default_lang = 'ja';
+	$conf->accept_langs = array('ja', 'en');
 
 	// commands
 	$conf->commands = new stdClass;
 	$conf->commands->php = 'php';
 
-	// processor
+	/**
+	 * paths_proc_type
+	 *
+	 * パスのパターン別に処理方法を設定します。
+	 *
+	 * - ignore = 対象外パス。Pickles 2 のアクセス可能範囲から除外します。このパスへのアクセスは拒絶され、パブリッシュの対象からも外されます。
+	 * - direct = 物理ファイルを、ファイルとして読み込んでから加工処理を通します。 (direct以外の通常の処理は、PHPファイルとして `include()` されます)
+	 * - pass = 物理ファイルを、そのまま無加工で出力します。 (デフォルト)
+	 * - その他 = extension名
+	 *
+	 * パターンは先頭から検索され、はじめにマッチした設定を採用します。
+	 * ワイルドカードとして "*"(アスタリスク) が使用可能です。
+	 * 部分一致ではなく、完全一致で評価されます。従って、ディレクトリ以下すべてを表現する場合は、 `/*` で終わるようにしてください。
+	 *
+	 * extensionは、 `$conf->funcs->processor` に設定し、設定した順に実行されます。
+	 * 例えば、 '*.html' => 'html' にマッチしたリクエストは、
+	 * $conf->funcs->processor->html に設定したプロセッサのリストに沿って、上から順に処理されます。
+	 */
 	$conf->paths_proc_type = array(
-		// パスのパターン別に処理方法を設定
-		//     - ignore = 対象外パス
-		//     - direct = 加工せずそのまま出力する(デフォルト)
-		//     - その他 = extension 名
-		// パターンは先頭から検索され、はじめにマッチした設定を採用する。
-		// ワイルドカードとして "*"(アスタリスク) を使用可。
 		'/.htaccess' => 'ignore' ,
 		'/.px_execute.php' => 'ignore' ,
 		'/px-files/*' => 'ignore' ,
-		'*.ignore/*' => 'ignore' ,
-		'*.ignore.*' => 'ignore' ,
 		'/composer.json' => 'ignore' ,
 		'/composer.lock' => 'ignore' ,
 		'/README.md' => 'ignore' ,
@@ -60,6 +73,12 @@ return call_user_func( function(){
 		'*/.svn/*' => 'ignore' ,
 		'*/.git/*' => 'ignore' ,
 		'*/.gitignore' => 'ignore' ,
+		'*.ignore/*' => 'ignore' ,
+		'*.ignore.*' => 'ignore' ,
+		'*.pass/*' => 'pass' ,
+		'*.pass.*' => 'pass' ,
+		'*.direct/*' => 'direct' ,
+		'*.direct.*' => 'direct' ,
 
 		'*.html' => 'html' ,
 		'*.htm' => 'html' ,
@@ -84,24 +103,25 @@ return call_user_func( function(){
 		)),
 
 		// PX=clearcache
-		'picklesFramework2\commands\clearcache::register' ,
+		'picklesFramework2\commands\clearcache::register',
 
-		 // PX=config
-		'picklesFramework2\commands\config::register' ,
+		// PX=config
+		'picklesFramework2\commands\config::register',
 
-		 // PX=phpinfo
-		'picklesFramework2\commands\phpinfo::register' ,
-
+		// PX=phpinfo
+		'picklesFramework2\commands\phpinfo::register',
 	];
 
 	// funcs: Before content
 	$conf->funcs->before_content = [
 		// PX=api
-		'picklesFramework2\commands\api::register' ,
+		'picklesFramework2\commands\api::register',
 
 		// PX=publish
 		'picklesFramework2\commands\publish::register' ,
 
+		// PX=px2dthelper
+		'tomk79\pickles2\px2dthelper\main::register',
 	];
 
 
@@ -110,13 +130,13 @@ return call_user_func( function(){
 
 	$conf->funcs->processor->html = [
 		// ページ内目次を自動生成する
-		'picklesFramework2\processors\autoindex\autoindex::exec' ,
+		'picklesFramework2\processors\autoindex\autoindex::exec',
 
 		// テーマ
 		'theme' => \tomk79\pickles2\multitheme\theme::exec(array(
 			'param_theme_switch'=>'THEME',
 			'cookie_theme_switch'=>'THEME',
-			'path_theme_collection'=>'./px-files/themes/',
+			'path_theme_collection'=>__DIR__.'/themes/',
 			'attr_bowl_name_by'=>'data-contents-area',
 			'default_theme_id'=>'standard',
 			'options'=>array(
@@ -127,36 +147,43 @@ return call_user_func( function(){
 		)),
 
 		// Apache互換のSSIの記述を解決する
-		'picklesFramework2\processors\ssi\ssi::exec' ,
+		'picklesFramework2\processors\ssi\ssi::exec',
+
+		// broccoli-receive-message スクリプトを挿入
+		'tomk79\pickles2\px2dthelper\broccoli_receive_message::apply('.json_encode( array(
+			// 許可する接続元を指定
+			'enabled_origin'=>array(
+			)
+		) ).')' ,
 
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
-		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
+		'picklesFramework2\processors\encodingconverter\encodingconverter::exec',
 	];
 
 	$conf->funcs->processor->css = [
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
-		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
+		'picklesFramework2\processors\encodingconverter\encodingconverter::exec',
 	];
 
 	$conf->funcs->processor->js = [
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
-		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
+		'picklesFramework2\processors\encodingconverter\encodingconverter::exec',
 	];
 
 	$conf->funcs->processor->md = [
 		// Markdown文法を処理する
-		'picklesFramework2\processors\md\ext::exec' ,
+		'picklesFramework2\processors\md\ext::exec',
 
 		// html の処理を追加
-		$conf->funcs->processor->html ,
+		$conf->funcs->processor->html,
 	];
 
 	$conf->funcs->processor->scss = [
 		// SCSS文法を処理する
-		'picklesFramework2\processors\scss\ext::exec' ,
+		'picklesFramework2\processors\scss\ext::exec',
 
 		// css の処理を追加
-		$conf->funcs->processor->css ,
+		$conf->funcs->processor->css,
 	];
 
 
@@ -166,20 +193,41 @@ return call_user_func( function(){
 
 
 
+	// config for Plugins.
+	$conf->plugins = new stdClass;
+
+
+
 	// -------- PHP Setting --------
 
-	// [memory_limit]
-	// PHPのメモリの使用量の上限を設定します。
-	// 正の整数値で上限値(byte)を与えます。
-	//     例: 1000000 (1,000,000 bytes)
-	//     例: "128K" (128 kilo bytes)
-	//     例: "128M" (128 mega bytes)
-	// -1 を与えた場合、無限(システムリソースの上限まで)に設定されます。
-	// サイトマップやコンテンツなどで、容量の大きなデータを扱う場合に調整してください。
-	// @ini_set( 'memory_limit' , -1 );
-	@ini_set( 'display_errors' , 'On' );
-	@ini_set( 'error_reporting' , 32767 );
+	/**
+	 * `memory_limit`
+	 *
+	 * PHPのメモリの使用量の上限を設定します。
+	 * 正の整数値で上限値(byte)を与えます。
+	 *
+	 *     例: 1000000 (1,000,000 bytes)
+	 *     例: "128K" (128 kilo bytes)
+	 *     例: "128M" (128 mega bytes)
+	 *
+	 * -1 を与えた場合、無限(システムリソースの上限まで)に設定されます。
+	 * サイトマップやコンテンツなどで、容量の大きなデータを扱う場合に調整してください。
+	 */
+	@ini_set( 'memory_limit' , -1 );
 
+	/**
+	 * `display_errors`, `error_reporting`
+	 *
+	 * エラーを標準出力するための設定です。
+	 *
+	 * PHPの設定によっては、エラーが発生しても表示されない場合があります。
+	 * もしも、「なんか挙動がおかしいな？」と感じたら、
+	 * 必要に応じてこれらのコメントを外し、エラー出力を有効にしてみてください。
+	 *
+	 * エラーメッセージは問題解決の助けになります。
+	 */
+	@ini_set('display_errors', 1);
+	@ini_set('error_reporting', E_ALL);
 
 
 	return $conf;
